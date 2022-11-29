b9221ca2c7be419d289c130ac5e91e4e193dba09
Priyansh Rathi <techiepriyansh@gmail.com>
2022-11-29 23:26:43 +0530

    add loading circle while waiting for super resolution

--- gui/views/js/super-resolution/messages.js
+++ gui/views/js/super-resolution/messages.js
@@ -1,11 +1,14 @@
 eel.expose(displayIUTforSuperResolution);
 function displayIUTforSuperResolution(message) {
-    const iutNode = document.getElementById('iut');
-    iutNode.src = message;
+    document.getElementById('iut-loader').style.display = "none";
+    document.getElementById('iut-zoom').style.display = "flex";
+    document.getElementById('iut').src = message;
 }
 
 eel.expose(displayOUTforSuperResolution);
 function displayOUTforSuperResolution(message) {
-    const outNode = document.getElementById('out');
-    outNode.src = message;
+    document.getElementById('out-loader').style.display = "none";
+    document.getElementById('out-zoom').style.display = "flex";
+    document.getElementById('out').src = message;
 }
+
